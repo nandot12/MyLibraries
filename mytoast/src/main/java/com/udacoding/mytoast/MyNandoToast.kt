34f1139659c14c0a497091fd0ca44354f519d4d9
udacodingid <mentoring@udacoding.com>
2021-10-29 20:40:27 +0700

    Initial commit

---
+++ mytoast/src/main/java/com/udacoding/mytoast/MyNandoToast.kt
@@ -0,0 +1,12 @@
+package com.udacoding.mytoast
+
+import android.content.Context
+import android.widget.Toast
+
+class MyNandoToast {
+
+
+    fun showToast(c : Context, msg : String){
+        Toast.makeText(c, msg, Toast.LENGTH_SHORT).show()
+    }
+}

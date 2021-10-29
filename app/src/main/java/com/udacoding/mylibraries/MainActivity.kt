3f4581d1e93574836dba83670cffdaed05f3e6de
udacodingid <mentoring@udacoding.com>
2021-10-29 22:47:35 +0700

    publish

--- app/src/main/java/com/udacoding/mylibraries/MainActivity.kt
+++ app/src/main/java/com/udacoding/mylibraries/MainActivity.kt
@@ -2,10 +2,14 @@ package com.udacoding.mylibraries
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.udacoding.mytoast.MyNandoToast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        MyNandoToast().showToast(this,"sj")
     }
 }
